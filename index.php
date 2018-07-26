2c81ccb17a367565c5447b8ec412eb0f71452d33
Sarah Van Oers <vanoerssarah@gmail.com>
2018-07-26 09:34:13 +0200

    login & register

--- index.php
+++ index.php
@@ -1,12 +1,48 @@
-<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Form  CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+
+    <title>Login form | To Do</title>
 </head>
 <body>
-    
+<div class="backgroundLogin">
+    <div id="whiteBgLogin" .shadow-* >
+    <div class="container">
+    <form method="POST">
+    <h1 class="logo">To Do app</h1>
+    <h1 class="tekstRegister">Hello you, Welcome back! Sign in.</h1>
+        <div class="form-group">
+            <label for="exampleInputEmail1"></label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" name="email">
+        </div>
+            <label for="exampleInputPassword1"></label>
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" name="password">
+        <?php if( isset($error) ): ?>
+            <div class="form-group">
+                <p>
+                    Sorry, we can't log you in with that email address and password. Can you try again?
+                </p>
+            </div>
+        <?php endif;?>
+        <div class="form-group">
+            <input type="submit" class="btn btn-primary" id="register" value="Let's get started!"><br>
+            <a id="loginLink" href="register.php">I'm new here, sign me in!</a>
+        </div>
+    </form>
+    </div>
+    </div>
+    </div>
 </body>
 </html>
---
+++ register.php
@@ -0,0 +1,49 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Form  CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+    <title>Register form | To Do</title>
+</head>
+<body>
+    <div class="backgroundLogin">
+    <div id="whiteBg">
+    <div class="container">
+        <form method="POST" class="form">
+            <h2 class="teksRegister">Hello you,<br> Sign up.</h2>
+            
+            <div class="form-group">
+                <label for="exampleInput"></label>
+                <input type="text" class="form-control" id="exampleInput" placeholder="Your first name" name="firstname">
+
+                <label for="exampleInput"></label>
+                <input type="text" class="form-control" id="exampleInput" placeholder="Your last name" name="lastname">
+
+                <label for="exampleInputEmail1"></label>
+                <input type="email" class="form-control" id="exampleInput" placeholder="Your email" name="email">
+
+                <label for="exampleInputPassword1"></label>
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Your password" name ="password">
+
+                <label for="exampleInputPasswordConfirmation1"></label>
+                <input type="password" class="form-control" id="exampleInputPasswordConfirmation1" placeholder="Repeat password" name ="password_confirmation">
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" id="register" value="Let's get started!"> <br>
+                <a id="registerLink" href="login.php">Oh, I already have one!</a>
+            </div>
+        </form>
+    </div>
+    </div>
+    </div>
+</body>
+</html>
