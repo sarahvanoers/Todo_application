df7581120caf83dddd9bd84754ef5a7d08c19408
Sarah Van Oers <vanoerssarah@gmail.com>
2018-08-10 15:24:50 +0200

    navbar check

--- home.php
+++ home.php
@@ -12,11 +12,11 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
-    <title>Home | To Do</title>
+    <title>Home | ToDo</title>
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="brand" id="navLogo" href="home.php">To do</a>
+        <a class="brand" id="navLogo" href="home.php">Todo</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
   <span class="navbar-toggler-icon">
     <i class="fa fa-navicon"></i>
@@ -29,14 +29,7 @@
             </div>
         </div>
     </nav>
-<div class="row">
-  <div class="col-4">
-    <h1 class="username">Hi Sarah Van Oers </h1>
-  </div>
-  <div class="col-8">
-  
-  </div>
-</div>
+
 
 
 
